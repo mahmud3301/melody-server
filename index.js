85a50ce2b18d9bc6337164c18da39cb99bb1be37
Mahmudul Hasan Mahi <mahmudulhasan13189@gmail.com>
2023-06-09 21:30:03 +0600

    user data store in db

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 
+// middleware
 app.use(cors());
 app.use(express.json());
 
@@ -28,26 +29,31 @@ async function run() {
     const classes = client.db("summerCamp").collection("classes");
     const instructors = client.db("summerCamp").collection("instructors");
     const carts = client.db("summerCamp").collection("carts");
+    const users = client.db("summerCamp").collection("users");
+    
+    // user related apis
+    app.post("/users", async (req, res) => {
+      
+    })
 
+    // classes api
     app.get("/classes", async (req, res) => {
       const result = await classes.find({}).toArray();
       res.send(result);
     });
 
+    // instructors api
     app.get("/instructors", async (req, res) => {
       const result = await instructors.find({}).toArray();
       res.send(result);
     });
 
     // cart related apis
-
     app.get("/carts", async (req, res) => {
       const { email } = req.query;
-
       if (!email) {
         res.send([]);
       }
-
       const query = { email: email };
       const result = await carts.find(query).toArray();
       res.send(result);
@@ -57,7 +63,14 @@ async function run() {
       const cart = req.body;
       const result = await carts.insertOne(cart);
       res.send(result);
-    })
+    });
+
+    app.delete("/carts/:id", async (req, res) => {
+      const { id } = req.params;
+      const query = { _id: new ObjectId(id) };
+      const result = await carts.deleteOne(query);
+      res.send(result);
+    });
 
     await client.db("admin").command({ ping: 1 });
     console.log(
