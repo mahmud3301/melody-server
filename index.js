e54f23fa69e208713e9d88a1151af8d58d07c5d6
Mahmudul Hasan Mahi <mahmudulhasan13189@gmail.com>
2023-06-11 19:25:03 +0600

    facing problems making payment intent

--- index.js
+++ index.js
@@ -227,16 +227,7 @@ async function run() {
 
     // create payment intent
     app.post("/create-payment-intent", verifyJWT, async (req, res) => {
-      const { price } = req.body;
-      const amount = parseInt(price * 100);
-      const paymentIntent = await stripe.paymentIntents.create({
-        amount: amount,
-        currency: "usd",
-        payment_method_types: ["card"],
-      });
-      res.send({
-        clientSecret: paymentIntent.client_secret,
-      });
+      
     });
 
     
